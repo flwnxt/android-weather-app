ba5caaeec7446e7c9340b8285e49ce192977dd5f
flwnxt <florescu.stefan96@gmail.com>
2026-01-07 13:09:30 +0200

    Added JS script for navbar to be visible everywhere

---
+++ app/shared/js/navbar.js
@@ -0,0 +1,16 @@
+
+
+// Inserare navbar in fiecare pagina 
+(async function loadNavbar() {
+  const container = document.getElementById("navbar-container");
+  if (!container) return;
+
+  const navbarPath = "../../shared/components/navbar.html";
+
+  const res = await fetch(navbarPath);
+  const html = await res.text();
+  container.innerHTML = html;
+})();
+
+
+
